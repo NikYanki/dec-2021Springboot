3ba902fb1e6511a4a1f5174c2087989c0f718ce9
NikYanki <nankovoj799@gmail.com>
2022-07-10 10:23:34 +0200

    Initial commit

---
+++ src/main/java/com/example/dec2021springboot/Dec2021SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.dec2021springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dec2021SpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Dec2021SpringbootApplication.class, args);
+    }
+
+}
